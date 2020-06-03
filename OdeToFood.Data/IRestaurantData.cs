8af0a34183bdd851c37adfa0a377e402e430cf95
Juan Rodas Manez <jrodas@analytics.pe>
2020-06-03 09:56:17 -0500

    Working with Models and Model Binding

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,44 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        //IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants = new List<Restaurant>();
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 2, Name = "American Pizza", Location = "New York", Cuisine = CuisineType.Mexican},
+                new Restaurant { Id = 3, Name = "Hawai Pizza", Location = "Manhattan", Cuisine = CuisineType.Italian},
+            };
+
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault( r => r.Id == id);
+        }
+    }
+}
